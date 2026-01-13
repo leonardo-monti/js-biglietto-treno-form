3be3bfb08717ff9522802bf56804f8857fac6d8f
leonardo-monti <monti.leonardo22@gmail.com>
2026-01-13 15:04:15 +0100

    milestone 1

---
+++ main.js
@@ -0,0 +1,16 @@
+document.getElementById("calcola").addEventListener("click", function () {
+
+
+    let km = Number(document.getElementById("km").value);
+    let eta = Number(document.getElementById("eta").value);
+
+    let prezzo = km * 0.21;
+
+    if (eta < 18) {
+        prezzo = prezzo * 0.8;
+    } else if (eta >= 65) {
+        prezzo = prezzo * 0.6;
+    }
+
+    console.log("Il prezzo del biglietto è: €" + prezzo.toFixed(2));
+});
